3f5ba12a5bb7c25ddd6c4d9971a75aad3fa484ee
Vatsalya Sharma <vatsalya2021.official@gmail.com>
2023-01-27 21:45:02 +0530

    project-page: Create projects page.
    Add an automatic image carousel displaying an image with a brief
    description of each project.
    
    Linters configured locally.
    
    Fixes: #8

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import './App.css';
+import './App.css'
 import Navbar from './components/Navbar'
 
-function App() {
+import Projects from './pages/projects/Projects'
+
+function App () {
   return (
-    <div className="App">
+    <div className='app-class'>
       <Navbar />
+      <Projects />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
