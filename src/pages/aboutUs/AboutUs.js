1c527523cbfedf6acdd8e28bf29c777d32be9811
Ekansh Verma <21bee083@nith.ac.in>
2023-02-05 19:16:03 +0530

    Frontend: Added About Us page
    Added the basic info of team ojas and co coordiantors
    
    Fixes:#4

--- src/pages/aboutUs/AboutUs.js
+++ src/pages/aboutUs/AboutUs.js
@@ -1,7 +1,48 @@
 import './AboutUs.css'
 
-export default function AboutUs () {
+export default function AboutUs() {
   return (
-    <div>AboutUs</div>
+    <div>
+
+
+      <div className="about-text">
+        <h1 className='aboutUs'>About Us</h1>
+        <p>
+          Team OJAS, the departmental team of Electrical Engineering at National Institute of Technology, Hamirpur is a club driven by the motto “Fuelled by innovation,
+          driven by determination” that strives to innovate viable and smart solutions for the many problems of our lives.
+          We conduct various technical as well as non-technical events throughout the year aimed at accelerating students interest
+          towards electrical engineering and nurturing a learning yet enjoyable atmosphere where everyone can exhibit their ideas and
+          get a chance to turn their dreams into reality. We have also won the prize in “Best Event Execution” category in NIMBUS 2021.</p>
+
+      </div>
+
+      <h2 className='cc-title'>Club Co-ordiantors of Team OJAS</h2>
+      <div className="aboutUs-cards">
+        <div className="profile-box">
+          <div className="profile-pic"></div>
+
+          <h3 className='cc-name'>Gaurav
+            <div className='socials'>
+              <a href="google.com" className="fa-brands fa-instagram"></a>
+              <a href="google.com" className="fa-brands fa-linkedin"></a>
+            </div>
+          </h3>
+        </div>
+        <div className="profile-box">
+          <div className="profile-pic"></div>
+          <h3 className='cc-name'>Gopanshu
+            <div className='socials'>
+              <a href="google.com" className="fa-brands fa-instagram"></a>
+              <a href="google.com" className="fa-brands fa-linkedin"></a>
+            </div>
+          </h3>
+
+        </div>
+
+
+      </div>
+      <div >
+      </div>
+    </div>
   )
 }
