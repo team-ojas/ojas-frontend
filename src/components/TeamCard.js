01de857e9e0263780cd4723fb20231b532180f8c
Author: Author: Vatsalya Sharma <vatsalya2021.official@gmail.com>
2023-02-13 23:55:21 +0530

    TeamCard: Add LinkedIn icon.
    This commit adds a permanent linkedin icon,
    redirecting to user linkedin profile.
    
    Fixes: #17

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -4,6 +4,9 @@ export default function TeamCard (props) {
   return (
     <div className='card'>
       <img className='card-img' src='https://www.w3schools.com/css/img_forest.jpg' alt='' />
+      <a className='socials' href='https://www.linkedin.com/feed/'>
+        <i className='linkedin fa-brands fa-linkedin fa-2xl' />
+      </a>
       <p className='card-name'>{props.name}</p>
     </div>
   )
