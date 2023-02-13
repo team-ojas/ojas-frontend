df1d55000249edb4e293003f7ef4cda0c87416c3
Author: Author: Vatsalya Sharma <vatsalya2021.official@gmail.com>
2023-02-13 23:31:14 +0530

    Navbar: Add className for navbar options.
    This commit adds a className for navbar options,
    instead of directly using anchor tag.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,11 +11,11 @@ export default function Navbar () {
         </label>
         <label className='logo'>Team OJAS</label>
         <ul>
-          <li><Link to='/'><i class='fa-solid fa-house' />Home</Link></li>
-          <li><Link to='projects'><i class='fa-solid fa-diagram-project' />Projects</Link></li>
-          <li><Link to='/team'><i class='fa-solid fa-people-group' />Team</Link></li>
-          <li><Link to='/aboutUs'><i class='fa-solid fa-circle-info' />About Us</Link></li>
-          <li><Link to='/events'><i class='fa-sharp fa-solid fa-calendar-days' />Events</Link></li>
+          <li><Link className='nav-options' to='/'><i class='fa-solid fa-house' />Home</Link></li>
+          <li><Link className='nav-options' to='projects'><i class='fa-solid fa-diagram-project' />Projects</Link></li>
+          <li><Link className='nav-options' to='/team'><i class='fa-solid fa-people-group' />Team</Link></li>
+          <li><Link className='nav-options' to='/aboutUs'><i class='fa-solid fa-circle-info' />About Us</Link></li>
+          <li><Link className='nav-options' to='/events'><i class='fa-sharp fa-solid fa-calendar-days' />Events</Link></li>
         </ul>
       </nav>
     </div>
