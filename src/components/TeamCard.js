e0665f928b3162d4326bc8be80300ff5a50d725a
Vatsalya Sharma <vatsalya2021.official@gmail.com>
2023-02-14 01:05:33 +0530

    TeamPage: Add TeamCard using loop.
    Add map method on TeamCard component.
    
    Fixes: #19

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -4,7 +4,7 @@ export default function TeamCard (props) {
   return (
     <div className='card'>
       <img className='card-img' src='https://www.w3schools.com/css/img_forest.jpg' alt='' />
-      <a className='socials' href='https://www.linkedin.com/feed/'>
+      <a className='socials' href={props.link}>
         <i className='linkedin fa-brands fa-linkedin fa-2xl' />
       </a>
       <p className='card-name'>{props.name}</p>
