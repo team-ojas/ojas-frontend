bcf5cf7fdcc3d27d263d17bef58f8602e77b0084
Vatsalya Sharma <vatsalya2021.official@gmail.com>
2023-02-14 21:33:41 +0530

    TeamCard: Add lazy loading to images.
    Added lazy loading effect for team-card images.
    
    This is required because we have ~ 100 team members.
    
    Fixes #21

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -1,9 +1,18 @@
 import './TeamCard.css'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
+import 'react-lazy-load-image-component/src/effects/black-and-white.css'
+import placeholderImg from './image.jpg'
 
 export default function TeamCard (props) {
   return (
     <div className='card'>
-      <img className='card-img' src='https://www.w3schools.com/css/img_forest.jpg' alt='' />
+      <LazyLoadImage
+        src={props.imageLink}
+        placeholderSrc={placeholderImg}
+        effect='black-and-white'
+        className='card-img'
+        alt='Profile Picture'
+      />
       <a className='socials' href={props.link}>
         <i className='linkedin fa-brands fa-linkedin fa-2xl' />
       </a>
--- src/pages/team/Team.js
+++ src/pages/team/Team.js
@@ -3,10 +3,10 @@ import './Team.css'
 
 export default function Team () {
   const members = [
-    { name: 'Khwab Kalra', link: 'https://developer.mozilla.org/en-US/' },
-    { name: 'Shubham Sharma', link: 'https://developer.mozilla.org/en-US/' },
-    { name: 'Prakhar', link: 'https://developer.mozilla.org/en-US/' },
-    { name: 'BHavesh', link: 'https://developer.mozilla.org/en-US/' }
+    { name: 'Khwab Kalra', link: 'https://developer.mozilla.org/en-US/', imageLink: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg' },
+    { name: 'Shubham Sharma', link: 'https://developer.mozilla.org/en-US/', imageLink: 'https://images.unsplash.com/photo-1597074866923-dc0589150358?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2hpbWxhfGVufDB8fDB8fA%3D%3D&w=1000&q=80' },
+    { name: 'Prakhar', link: 'https://developer.mozilla.org/en-US/', imageLink: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg' },
+    { name: 'BHavesh', link: 'https://developer.mozilla.org/en-US/', imageLink: 'https://cdn.pixabay.com/photo/2021/08/26/01/56/mountains-6574828__340.jpg' }
   ]
 
   return (
@@ -17,7 +17,7 @@ export default function Team () {
       </div>
 
       <div className='flex-container'>
-        {members.map((member, index) => <TeamCard key={index} name={member.name} link={member.link} />)}
+        {members.map((member, index) => <TeamCard key={index} name={member.name} link={member.link} imageLink={member.imageLink} />)}
       </div>
     </div>
   )
