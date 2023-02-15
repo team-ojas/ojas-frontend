9e41609b4956976656879074e8a69db27f897e5b
Ekansh Verma <21bee083@nith.ac.in>
2023-02-15 23:38:58 +0530

    Frontend: Added event page
    Added events page
    
    Fixes: #7

--- src/pages/aboutUs/AboutUs.js
+++ src/pages/aboutUs/AboutUs.js
@@ -1,7 +1,30 @@
 import './AboutUs.css'
+import TeamCard from '../../components/TeamCard'
 
 export default function AboutUs () {
   return (
-    <div>AboutUs</div>
+    <div>
+
+      <div className='about-text'>
+        <h1 className='about-us'>About Us</h1>
+        <p>
+          Team OJAS, the departmental team of Electrical Engineering at National Institute of Technology, Hamirpur is a club driven by the motto “Fuelled by innovation,
+          driven by determination” that strives to innovate viable and smart solutions for the many problems of our lives.
+          We conduct various technical as well as non-technical events throughout the year aimed at accelerating students interest
+          towards electrical engineering and nurturing a learning yet enjoyable atmosphere where everyone can exhibit their ideas and
+          get a chance to turn their dreams into reality. We have also won the prize in “Best Event Execution” category in NIMBUS 2021.
+        </p>
+
+      </div>
+
+      <h2 className='cc-title'>Club Co-ordiantors of Team OJAS</h2>
+      <div className='about-us-cards'>
+
+        <TeamCard name='Gopanshu' />
+        <TeamCard name='Gaurav' />
+
+      </div>
+      <div />
+    </div>
   )
 }
--- src/pages/events/Events.js
+++ src/pages/events/Events.js
@@ -2,6 +2,80 @@ import './Events.css'
 
 export default function Events () {
   return (
-    <div>Events</div>
+    <div>
+      <h1 className='page-title'>
+        Events by TEAM OJAS
+      </h1>
+      <div className='event-cards'>
+        <div className='blog-item-1'>
+          <a href='index.html'>
+            <div className='event-icon' />
+            <div className='content'>
+              <div className='title'> Design Detective  <span className='blog-date'>| 06.02.2023</span></div>
+              <div className='rounded' />
+
+              <p className='content-1'>
+                Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı
+                bilinmeyen bir matbaacının bir hurufat numune kitabı...
+              </p>
+            </div>
+
+          </a>
+
+        </div>
+
+        <div className='blog-item-2'>
+          <a href='index.html'>
+            <div className='event-icon' />
+            <div className='content'>
+              <div className='title'>Feel The Spark <span className='blog-date'>| 27.12.2022</span></div>
+              <div className='rounded' />
+
+              <p className='content-2'>
+                Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı
+                bilinmeyen bir matbaacının bir hurufat numune kitabı...
+              </p>
+            </div>
+
+          </a>
+
+        </div>
+
+        <div className='blog-item-3'>
+          <a href='index.html'>
+            <div className='event-icon' />
+            <div className='content'>
+              <div className='title'>Lorem Ipsum <span className='blog-date'>| 27.12.2022</span></div>
+              <div className='rounded' />
+
+              <p className='content-3'>
+                Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı
+                bilinmeyen bir matbaacının bir hurufat numune kitabı...
+              </p>
+            </div>
+
+          </a>
+
+        </div>
+
+        <div className='blog-item-4'>
+          <a href='index.html'>
+            <div className='event-icon' />
+            <div className='content'>
+              <div className='title'>Lorem Ipsum <span className='blog-date'>| 27.12.2017</span></div>
+              <div className='rounded' />
+
+              <p className='content-4'>
+                Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı
+                bilinmeyen bir matbaacının bir hurufat numune kitabı...
+              </p>
+            </div>
+
+          </a>
+
+        </div>
+
+      </div>
+    </div>
   )
 }
