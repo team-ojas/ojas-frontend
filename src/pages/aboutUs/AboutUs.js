028a863a50b9e3178b65d63f43b5ee97f2c7f427
Vatsalya Sharma <vatsalya2021.official@gmail.com>
2023-02-16 18:12:49 +0530

    Frontend: Added About Us Page
    Added about us page with the layout for the text, images, logo.
    
    Fixes: #24

--- src/pages/aboutUs/AboutUs.js
+++ src/pages/aboutUs/AboutUs.js
@@ -1,7 +1,28 @@
 import './AboutUs.css'
+import TeamCard from '../../components/TeamCard'
+import Logo from './ojas-logo.jpg'
 
 export default function AboutUs () {
+  const members = [
+    { name: 'Bhavesh', link: 'https://developer.mozilla.org/en-US/', imageLink: 'https://cdn.pixabay.com/photo/2021/08/26/01/56/mountains-6574828__340.jpg' },
+    { name: 'Bhavesh', link: 'https://developer.mozilla.org/en-US/', imageLink: 'https://cdn.pixabay.com/photo/2021/08/26/01/56/mountains-6574828__340.jpg' }
+  ]
+
   return (
-    <div>AboutUs</div>
+    <div className='about-us-div'>
+      <div className='about-body'>
+        <div className='about-content'>
+          <h1 style={{ marginBottom: '30px' }}>About Us</h1>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam qui libero, nulla quo sequi doloribus dicta, ea similique voluptatum minus voluptates architecto beatae quam eum nam fugiat. Soluta, saepe ipsa?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam qui libero, nulla quo sequi doloribus dicta, ea similique voluptatum minus voluptates architecto beatae quam eum nam fugiat. Soluta, saepe ipsa?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam qui libero, nulla quo sequi doloribus dicta, ea similique voluptatum minus voluptates architecto beatae quam eum nam fugiat. Soluta, saepe ipsa?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam qui libero, nulla quo sequi doloribus dicta, ea similique voluptatum minus voluptates architecto beatae quam eum nam fugiat. Soluta, saepe ipsa?</p>
+        </div>
+        <div className='cc-cards'>
+          {members.map((member, index) => <TeamCard key={index} name={member.name} link={member.link} imageLink={member.imageLink} />)}
+        </div>
+      </div>
+
+      <div className='logo-img-div'>
+        <img className='logo-image' src={Logo} alt='' />
+      </div>
+    </div>
   )
 }
