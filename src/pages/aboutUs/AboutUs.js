978bf16fc5d67bd7866e985ebf6f20be4e77802a
Vatsalya Sharma <vatsalya2021.official@gmail.com>
2023-02-16 21:58:58 +0530

    AboutUs: Add about us page.
    Add about us page with a team image and description.
    
    Fixes: #24

--- src/pages/aboutUs/AboutUs.js
+++ src/pages/aboutUs/AboutUs.js
@@ -2,6 +2,12 @@ import './AboutUs.css'
 
 export default function AboutUs () {
   return (
-    <div>AboutUs</div>
+    <div>
+      <div className='about-us-content'>
+        <h1 style={{ paddingTop: '2rem', textAlign: 'center' }}>About Us</h1>
+        <img className='team-img' src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg' alt='' />
+        <p style={{ paddingBottom: '3rem' }}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam qui libero, nulla quo sequi doloribus dicta, ea similique voluptatum minus voluptates architecto beatae quam eum nam fugiat. Soluta, saepe ipsa?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam qui libero, nulla quo sequi doloribus dicta, ea similique voluptatum minus voluptates architecto beatae quam eum nam fugiat. Soluta, saepe ipsa</p>
+      </div>
+    </div>
   )
 }
