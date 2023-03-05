3f23e7bf655def6710c6751fb2653042367f1016
Vatsalya Sharma <vatsalya2021.official@gmail.com>
2023-03-05 23:44:10 +0530

    Navbar:  Fix minor bugs.
    Add close functionality when toggler is clicked.
    Change the direction of sliding transition.
    Made the navbar fixed for scrolling user experience.
    Add link to home page when clicked on brand name.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,10 @@ import { Link } from 'react-router-dom'
 import './Navbar.css'
 
 export default function Navbar () {
+  const componentDidMount = () => {
+    document.querySelector('#check').checked = false
+  }
+
   return (
     <div>
       <nav>
@@ -9,13 +13,13 @@ export default function Navbar () {
         <label htmlFor='check' className='checkbtn'>
           <i className='fas fa-bars' />
         </label>
-        <label className='logo'>Team OJAS</label>
+        <label className='logo'><Link to='/'>Team OJAS</Link></label>
         <ul>
-          <li><Link className='nav-options' to='/'><i className='fa-solid fa-house' />Home</Link></li>
-          <li><Link className='nav-options' to='projects'><i className='fa-solid fa-diagram-project' />Projects</Link></li>
-          <li><Link className='nav-options' to='/team'><i className='fa-solid fa-people-group' />Team</Link></li>
-          <li><Link className='nav-options' to='/aboutUs'><i className='fa-solid fa-circle-info' />About Us</Link></li>
-          <li><Link className='nav-options' to='/events'><i className='fa-sharp fa-solid fa-calendar-days' />Events</Link></li>
+          <li><Link className='nav-options' to='/' onClick={componentDidMount}><i className='fa-solid fa-house' />Home</Link></li>
+          <li><Link className='nav-options' to='projects' onClick={componentDidMount}><i className='fa-solid fa-diagram-project' />Projects</Link></li>
+          <li><Link className='nav-options' to='/team' onClick={componentDidMount}><i className='fa-solid fa-people-group' />Team</Link></li>
+          <li><Link className='nav-options' to='/aboutUs' onClick={componentDidMount}><i className='fa-solid fa-circle-info' />About Us</Link></li>
+          <li><Link className='nav-options' to='/events' onClick={componentDidMount}><i className='fa-sharp fa-solid fa-calendar-days' />Events</Link></li>
         </ul>
       </nav>
     </div>
