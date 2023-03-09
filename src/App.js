4a185a468919260808c5d4e7c643bc7e9de5ab05
Vatsalya Sharma <vatsalya2021.official@gmail.com>
2023-03-10 00:22:09 +0530

    Background: Add a CSS based background animation.
    This commit adds glowing dots animation moving
    vertically upwards.

--- src/App.js
+++ src/App.js
@@ -14,6 +14,46 @@ import {
 function App () {
   return (
     <div className='app-class'>
+      <div className='animation-container'>
+        <div className='bubbles'>
+          <span style={{ '--i': '11' }} />
+          <span style={{ '--i': '12' }} />
+          <span style={{ '--i': '24' }} />
+          <span style={{ '--i': '10' }} />
+          <span style={{ '--i': '14' }} />
+          <span style={{ '--i': '23' }} />
+          <span style={{ '--i': '18' }} />
+          <span style={{ '--i': '16' }} />
+          <span style={{ '--i': '19' }} />
+          <span style={{ '--i': '20' }} />
+          <span style={{ '--i': '22' }} />
+          <span style={{ '--i': '25' }} />
+          <span style={{ '--i': '18' }} />
+          <span style={{ '--i': '21' }} />
+          <span style={{ '--i': '11' }} />
+          {/* <span style={{ '--i': '12' }}></span>
+        <span style={{ '--i': '18' }}></span>
+        <span style={{ '--i': '15' }}></span>
+        <span style={{ '--i': '11' }}></span>
+        <span style={{ '--i': '12' }}></span>
+        <span style={{ '--i': '24' }}></span>
+        <span style={{ '--i': '10' }}></span>
+        <span style={{ '--i': '14' }}></span>
+        <span style={{ '--i': '23' }}></span>
+        <span style={{ '--i': '18' }}></span>
+        <span style={{ '--i': '16' }}></span>
+        <span style={{ '--i': '19' }}></span>
+        <span style={{ '--i': '20' }}></span>
+        <span style={{ '--i': '22' }}></span>
+        <span style={{ '--i': '25' }}></span>
+        <span style={{ '--i': '18' }}></span>
+        <span style={{ '--i': '21' }}></span>
+        <span style={{ '--i': '11' }}></span>
+        <span style={{ '--i': '12' }}></span>
+        <span style={{ '--i': '18' }}></span>
+        <span style={{ '--i': '15' }}></span> */}
+        </div>
+      </div>
       <Router>
         <Navbar />
         <Routes>
