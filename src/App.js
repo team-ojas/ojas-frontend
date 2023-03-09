cbce5c82ab2cfaad3405feb476bf0cdf64109881
Vatsalya Sharma <vatsalya2021.official@gmail.com>
2023-03-09 16:54:21 +0530

    Frontend: Added VantaJS background
    Adde VantaJS BIRDS background and fixed the navbar and about us to be
    used along with VantaJS

--- src/App.js
+++ src/App.js
@@ -10,19 +10,57 @@ import {
   Routes,
   Route
 } from 'react-router-dom'
+import { useState, useEffect, useRef } from 'react'
+import BIRDS from 'vanta/dist/vanta.birds.min'
+import * as THREE from 'three'
 
 function App () {
+  const [vantaEffect, setVantaEffect] = useState(0)
+  const myRef = useRef(null)
+
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        BIRDS({
+          el: '#app-class',
+          THREE,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.00,
+          scale: 1.00,
+          scaleMobile: 1.00,
+          backgroundColor: '#242424',
+          color1: '#a259ff',
+          color2: '#df00ff',
+          wingSpan: 21.00,
+          separation: 63.00,
+          alignment: 65.00,
+          cohesion: 72.00,
+          quantity: 3.00
+        })
+      )
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy()
+    }
+  }, [vantaEffect])
+
   return (
-    <div className='app-class'>
+    <div>
       <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route exact path='/projects' element={<Projects />} />
-          <Route exact path='/events' element={<Events />} />
-          <Route exact path='/team' element={<Team />} />
-          <Route exact path='/aboutUs' element={<AboutUs />} />
-        </Routes>
+        <div ref={myRef} className='app-class' id='app-class'>
+          <div className='nav-div'>
+            <Navbar />
+          </div>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/projects' element={<Projects />} />
+            <Route exact path='/events' element={<Events />} />
+            <Route exact path='/team' element={<Team />} />
+            <Route exact path='/aboutUs' element={<AboutUs />} />
+          </Routes>
+        </div>
       </Router>
     </div>
   )
