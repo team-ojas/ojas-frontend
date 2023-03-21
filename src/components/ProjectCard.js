cafa766e6542f3259ee68077e66dddb560c13f68
Vatsalya Sharma <vatsalya2021.official@gmail.com>
2023-03-21 19:37:41 +0530

    Frontend: Added lazy loading
    Added lazy loading effect to projects as well as events.

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,9 +1,18 @@
 import './ProjectCard.css'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
+import 'react-lazy-load-image-component/src/effects/black-and-white.css'
+import placeholderImg from './image.jpg'
 
 export default function ProjectCard (props) {
   return (
     <div className='project-card'>
-      <img className='project-image' src={props.projectImage} alt='' />
+      <LazyLoadImage
+        src={props.projectImage}
+        placeholderSrc={placeholderImg}
+        effect='black-and-white'
+        className='project-image'
+        alt='Project Images'
+      />
       <h3 className='project-heading'>{props.projectName}</h3>
       <p className='project-desc'>{props.projectDesc}</p>
     </div>
