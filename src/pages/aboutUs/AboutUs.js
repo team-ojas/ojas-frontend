a98d8242d3e4b6e363297299d74d63a3de09d360
Vatsalya Sharma <vatsalya2021.official@gmail.com>
2023-04-07 12:37:35 +0530

    Frontend: Added all the content
    Added all the content of Team, Project, Events, AboutUs pages.
    Also modified CSS and touch ups.

--- src/pages/aboutUs/AboutUs.js
+++ src/pages/aboutUs/AboutUs.js
@@ -5,8 +5,8 @@ export default function AboutUs () {
     <div>
       <div className='about-us-content'>
         <h1 style={{ paddingTop: '2rem', textAlign: 'center' }}>About Us</h1>
-        <img className='team-img' src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg' alt='' />
-        <p style={{ paddingBottom: '3rem' }}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam qui libero, nulla quo sequi doloribus dicta, ea similique voluptatum minus voluptates architecto beatae quam eum nam fugiat. Soluta, saepe ipsa?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam qui libero, nulla quo sequi doloribus dicta, ea similique voluptatum minus voluptates architecto beatae quam eum nam fugiat. Soluta, saepe ipsa</p>
+        <img className='team-img' src='https://i.imgur.com/vzdEUcE.jpg' alt='' />
+        <p style={{ paddingBottom: '2rem' }}>Team OJAS is the Electrical Engineering department's club at the National Institute of Technology in Hamirpur. The club's motto is "Fueled by creativity, driven by determination," and its members work hard to come up with innovative and feasible answers to the world's pressing issues. We host both technical and non-technical activities all year round with the goals of increasing students' enthusiasm for electrical engineering and creating a welcoming space where everyone feels comfortable sharing their ideas and having a go at making them a reality. We also took home the top honour for "Best Event Execution" at the NIMBUS 2021 conference. </p>
       </div>
     </div>
   )
--- src/pages/events/Events.js
+++ src/pages/events/Events.js
@@ -3,23 +3,47 @@ import './Events.css'
 
 export default function Events () {
   const events = [
-    { eventName: 'RoboCarrom0', eventDesc: '1Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, nulla dolores. Fugiat magnam fuga, aspernatur debitis delectus optio minus consectetur qui aut dolore expedita rerum quibusdam rem ab iste. Aliquam hhjoooooooooooooooooooooooooooo', eventImage: 'https://media.istockphoto.com/id/611065352/photo/hills-and-clouds.jpg?s=612x612&w=0&k=20&c=chTA9wiQxovOYfy1AH0ejqfNBaHC5OIuGtxc2pd9LUw=', eventDate: '11/02/2003' },
-    { eventName: 'RoboCarrom1', eventDesc: '2Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, nulla dolores. Fugiat magnam fuga, aspernatur debitis delectus optio minus consectetur qui aut dolore expedita rerum quibusdam rem ab iste. Aliquam', eventImage: 'https://media.istockphoto.com/id/611065352/photo/hills-and-clouds.jpg?s=612x612&w=0&k=20&c=chTA9wiQxovOYfy1AH0ejqfNBaHC5OIuGtxc2pd9LUw=', eventDate: '11/02/2004' },
-    { eventName: 'RoboCarrom2', eventDesc: '3Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, nulla dolores. Fugiat magnam fuga, aspernatur debitis delectus optio minus consectetur qui aut dolore expedita rerum quibusdam rem ab iste. Aliquam', eventImage: 'https://media.istockphoto.com/id/611065352/photo/hills-and-clouds.jpg?s=612x612&w=0&k=20&c=chTA9wiQxovOYfy1AH0ejqfNBaHC5OIuGtxc2pd9LUw=', eventDate: '11/02/2005' },
-    { eventName: 'RoboCarrom3', eventDesc: '4Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, nulla dolores. Fugiat magnam fuga, aspernatur debitis delectus optio minus consectetur qui aut dolore expedita rerum quibusdam rem ab iste. Aliquam', eventImage: 'https://media.istockphoto.com/id/611065352/photo/hills-and-clouds.jpg?s=612x612&w=0&k=20&c=chTA9wiQxovOYfy1AH0ejqfNBaHC5OIuGtxc2pd9LUw=', eventDate: '11/02/2006' },
-    { eventName: 'RoboCarrom4', eventDesc: '5Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, nulla dolores. Fugiat magnam fuga, aspernatur debitis delectus optio minus consectetur qui aut dolore expedita rerum quibusdam rem ab iste. Aliquam', eventImage: 'https://media.istockphoto.com/id/611065352/photo/hills-and-clouds.jpg?s=612x612&w=0&k=20&c=chTA9wiQxovOYfy1AH0ejqfNBaHC5OIuGtxc2pd9LUw=', eventDate: '11/02/2007' },
-    { eventName: 'RoboCarrom5', eventDesc: '6Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, nulla dolores. Fugiat magnam fuga, aspernatur debitis delectus optio minus consectetur qui aut dolore expedita rerum quibusdam rem ab iste. Aliquam', eventImage: 'https://media.istockphoto.com/id/611065352/photo/hills-and-clouds.jpg?s=612x612&w=0&k=20&c=chTA9wiQxovOYfy1AH0ejqfNBaHC5OIuGtxc2pd9LUw=', eventDate: '11/02/2008' }
+    {
+      eventName: 'WIRED UP',
+      eventDesc: 'The name itself suggests that this fascinating game is all set in the vicinity of integrated circuits. This electrifying game will help you unravel the many secrets and have fun with circuits.',
+      eventImage: 'https://i.imgur.com/BSLmZ4R.jpeg',
+      eventDate: 'TBA'
+    },
+    {
+      eventName: 'Guest Lecture',
+      eventDesc: 'Chandan Jha sir is an Ex.ISRO Scientist, senior consultant of GATE/ESE at Physics Wallah, qualified GATE 6 times with AIR 23, & 26.',
+      eventImage: 'https://i.imgur.com/H1W9C29.png',
+      eventDate: '24/03/2023'
+    },
+    {
+      eventName: 'Robo Carrom',
+      eventDesc: ' Robo Carrom is an event wherein the two teams will compete with each other, having two bot cars each. Students will get exposure to using Arduino as the main controller. The winning team will be awarded prize money.',
+      eventImage: 'https://i.imgur.com/WAZC5Pg.jpg',
+      eventDate: 'TBA'
+    },
+    {
+      eventName: 'Crime Time: True Detective',
+      eventDesc: ' A situation-based event about a crime or an accident involving the use of all technical and general knowledge of students to solve it It will be in two stages. The winning team will receive prize money.',
+      eventDate: '24/03/2023',
+      eventImage: 'https://i.imgur.com/QdMjKfU.png'
+    },
+    {
+      eventName: 'Design Detective',
+      eventDesc: ' Unveiling of the new logo of team Ojas was inaugurated by the event. Each team participating in treasure hunt was given a puzzle through a QR code,the quickest team was declared winner.Details of the event: Maximum students per team: 3 Total students participating: 93 ,Prize: A Netflix subscription',
+      eventImage: 'https://i.imgur.com/tAi8KTH.png',
+      eventDate: '4/02/2003'
+    }
   ]
 
   return (
     <div className='events-page'>
       <div className='content'>
         <h1 style={{ marginBottom: '30px' }}>Events</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam qui libero, nulla quo sequi doloribus dicta, ea similique voluptatum minus voluptates architecto beatae quam eum nam fugiat. Soluta, saepe ipsa?</p>
+        <p>Keeping the enthusiasm of everyone high, we bring forth a plethora of fun filled events that push our imagination to a whole new level.</p>
       </div>
 
       <div className='events-container'>
-        {events.map((event, index) => <EventCard key={index} eventName={event.eventName} eventDesc={event.eventDesc} eventImage={event.eventImage} />)}
+        {events.map((event, index) => <EventCard key={index} eventName={event.eventName} eventDesc={event.eventDesc} eventImage={event.eventImage} eventDate={event.eventDate} />)}
       </div>
     </div>
   )
