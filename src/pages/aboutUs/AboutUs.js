e2af7dbae16deaf4616b6f895f729db4743a6b62
Vatsalya Sharma <vatsalya2021.official@gmail.com>
2023-04-08 18:20:51 +0530

    Frontend: Social media links and lazy loading on About US
    Added links to Instagram, Linkedin, Github and enabled Lazyloading on
    about us page

--- src/pages/aboutUs/AboutUs.js
+++ src/pages/aboutUs/AboutUs.js
@@ -1,12 +1,25 @@
 import './AboutUs.css'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
+import 'react-lazy-load-image-component/src/effects/black-and-white.css'
+import placeholderImg from '../../components/image.jpg'
 
 export default function AboutUs () {
   return (
     <div>
       <div className='about-us-content'>
         <h1 style={{ paddingTop: '2rem', textAlign: 'center' }}>About Us</h1>
-        <img className='team-img' src='https://i.imgur.com/vzdEUcE.jpg' alt='' />
+        {/* <img className='team-img' src='https://i.imgur.com/vzdEUcE.jpg' alt='' /> */}
+        <LazyLoadImage
+          src='https://i.imgur.com/vzdEUcE.jpg'
+          placeholderSrc={placeholderImg}
+          effect='black-and-white'
+          className='team-img'
+          alt='Ojas-team'
+        />
         <p className='ojas-about'>Team OJAS is the Electrical Engineering department's club at the National Institute of Technology in Hamirpur. The club's motto is "Fueled by creativity, driven by determination," and its members work hard to come up with innovative and feasible answers to the world's pressing issues. We host both technical and non-technical activities all year round with the goals of increasing students' enthusiasm for electrical engineering and creating a welcoming space where everyone feels comfortable sharing their ideas and having a go at making them a reality. We also took home the top honour for "Best Event Execution" at the NIMBUS 2021 conference. </p>
+        <a className='ojas-socials' href='https://www.instagram.com/team_ojas_nith/?hl=en'><i class='fa-brands fa-instagram fa-xl' /></a>
+        <a className='ojas-socials' href='https://www.linkedin.com/company/ojasnith/mycompany/'><i class='fa-brands fa-linkedin fa-xl' /></a>
+        <a className='ojas-socials' href='https://github.com/team-ojas'><i class='fa-brands fa-github fa-xl' /></a>
       </div>
     </div>
   )
